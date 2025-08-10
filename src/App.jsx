2fb622a2225eca5ed20176915e195de25cda6c50
Fenil-1 <fenilp2046@gmail.com>
2025-08-10 12:10:43 -0700

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,46 @@ import { useEffect, useState } from "react";
 import {Container,Paper,Typography,Stack,IconButton,Button,Box} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import TikTok from "./components/TikTok";
+import BreakNsession from "./components/BreakNsession";
+import Controls from "./components/Controls";
 
 function App() {
-  const [totalSec , setTotalSec] = useState(8);
-  let min =  Math.floor(totalSec / 60);
-  let sec = totalSec % 60;
-  useEffect(()=>{
-    const interval = setInterval(()=>{
-      setTotalSec((prev)=>{
-        if(prev!=0){
-          return prev-1;
-        }
-        else{
-          const aud = new Audio('/alert/raceTimer.mp3');
-          aud.play();
-          return clearInterval(interval);
-        }
-      })  
-    },1000);
-    return ()=>clearInterval(interval);
-  },[])
+  
+  const [totalSec, setTotalSec] = useState(1500);
+  const [isSession , setIsSession] = useState(false);
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [pause,setPause] = useState(false)
+  
+  useEffect(() => {
+      let interval;
+      const startTimer = (duration) => {
+        setTotalSec(duration);
+        interval = setInterval(() => {
+          
+          setTotalSec((prev) => {
+            if (prev >= 1) {
+              return prev - 1;    
+            } 
+            else {
+              clearInterval(interval); 
+              const aud = new Audio("/alert/raceTimer.mp3");
+              aud.play();
+              setTotalSec(0);
+              setIsSession(!isSession) ;
+              const nextDuration = isSession ? breakLength*60 : sessionLength*60;
+              startTimer(nextDuration);
+            }
+          });
+        }, 1000);
+      }
+      startTimer(totalSec);
+  
+      return () => clearInterval(interval);
+    }, []); 
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setSec((prevSec) => {
-  //       if (prevSec > 0) {
-  //         return prevSec - 1;
-  //       } else {
-  //         setMin((prevMin) => {
-  //           if (prevMin > 0) {
-  //             setSec(59);
-  //             return prevMin - 1;
-  //           } else {
-  //             clearInterval(interval);
-  //             return 0;
-  //           }
-  //         });
-         
-  //       }
-  //     });
-  //   }, 1000);
-  //   return () => clearInterval(interval);
-  // }, []);
-
- return (
+  return (
     <Container
       maxWidth="sm"
       sx={{
@@ -65,47 +61,16 @@ function App() {
         }}
       >
         <Typography variant="h4" gutterBottom>
-          Pomodoro Timer
+          25+5 Timer
         </Typography>
 
-        {/* Timer Display */}
-        <Typography variant="h2" sx={{ fontWeight: "bold", mb: 4 }}>
-          {`${String(min).padStart(2,'0')} : ${String(sec).padStart(2,'0')}`}
-        </Typography>
+     
+        <TikTok totalSec={totalSec} />
 
-        {/* Control Buttons */}
-        <Stack direction="row" spacing={2} justifyContent="center" mb={3}>
-          <Button variant="contained" color="primary">Pause</Button>
-          <Button variant="outlined" color="error">Reset</Button>
-        </Stack>
-
-        {/* Break Length Controls */}
-        <Box sx={{ mb: 3 }}>
-          <Typography variant="h6">Break Length</Typography>
-          <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
-            <IconButton color="primary">
-              <RemoveIcon />
-            </IconButton>
-            <Typography variant="h5">5</Typography>
-            <IconButton color="primary">
-              <AddIcon />
-            </IconButton>
-          </Stack>
-        </Box>
-
-        {/* Session Length Controls */}
-        <Box>
-          <Typography variant="h6">Session Length</Typography>
-          <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
-            <IconButton color="primary">
-              <RemoveIcon />
-            </IconButton>
-            <Typography variant="h5">20</Typography>
-            <IconButton color="primary">
-              <AddIcon />
-            </IconButton>
-          </Stack>
-        </Box>
+        <Controls setPause={setPause} setTotalSec={setTotalSec} setSessionLength={setSessionLength} />
+        
+        <BreakNsession pause={pause} setBreakLength={setBreakLength} breakLength={breakLength} setSessionLength={setSessionLength} sessionLength={sessionLength} />
+       
       </Paper>
     </Container>
   );
